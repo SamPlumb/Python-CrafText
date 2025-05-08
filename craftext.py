41f4814fe098e19d752f2d4576e2f003828d35ea
Sam <sam.plumb@outlook.com>
2025-05-08 20:47:45 +0100

    Added - Intro, Controls Menu, Wood Chop
    - Scenery description on game start
    
    - Bring up the Controls Menu when in-game
    
    - Added wood chopping, with and without an axe. randint to randomise the amount of wood gained.

--- craftext.py
+++ craftext.py
@@ -1,26 +1,20 @@
 import time
+import textwrap
+import random
 
 # CrafText -
 
 
 # Variables -
 char_len_of_ctrs = []
+qty_added = 0
+
 
 main_menu = True
 display_intro = True
 
 game_running = False
-
-
-# Items -
-log = 0
-plank = 0
-stick = 0
-stone = 0
-iron = 0
-gold = 0
-pick_dur = 0
-axe_dur = 0
+game_start = True
 
 # Box Ascii -
 
@@ -34,12 +28,30 @@ bot_left = "╚"
 bot_right = "╝"
 
 
-ctrs_list = ["Controls",
+# Items -
+
+plr_inv = {"Logs": 0,
+           "Planks": 0,
+           "Stick": 0,
+           "Stone": 0,
+           "Coal": 3,
+           "Iron Ore": 0,
+           "Iron Ingot": 0,
+           "Gold": 0,
+           "Wooden Pickaxe": 0,
+           "Stone Pickaxe": 0,
+           "Iron Pickaxe": 0,
+           "Stone Axe": 1}
+
+
+ctrs_list = ["Controls / Player Choices",
              " ",
-             "[Chop] - Chop down a tree",
+             "[controls] - Open this menu (Controls / Player Choices)",
+             "[Chop] - Chop down a tree.",
              "[Mine] - Mine rock with chance to find stone or minerals.",
-             "[Craft] - Open Crafting Menu",
-             "[Smelt] - Open Smelting Menu"]
+             "[Craft] - Open crafting menu.",
+             "[Smelt] - Open smelting menu.",
+             "[Bag] - See what items you have."]
 
 quit_text = "Thank you for playing!"
 
@@ -56,6 +68,8 @@ width_ctrs = max(char_len_of_ctrs)
 def print_c_str(text, padding=100, char_fill=" "):
     text = text.center(padding, char_fill)
     print(text)
+
+# def print_multi_line(text, width=100, )
 
 
 def ctrs_menu():
@@ -103,20 +117,118 @@ while main_menu:
         game_running = True
 
     else:
-        print_c_str("Data entered is invalid, Try again")
+        print_c_str("Action entered is invalid, Try again!")
 
 
 while game_running:
 
-    print_c_str("Welcome to the CrafText Wilderness")
+    if game_start == True:
 
-    if input().lower() == "quit":
+        print_c_str(f"{top_left}{top_bot*98}{top_right}")
+
+        for line in textwrap.wrap("Ahead of you, a peaceful river meanders through the valley, its clear waters reflecting the soft glow of the sky. The banks are lined with tall, sturdy trees, creating a quiet rhythm with every whisper of wind. In the distance, mountains pierce the sky with snow-capped peaks, while their lower slopes are covered in a blanket of pine trees.", width=96):
+            print(f"║ {line.center(96)} ║")
+
+        print_c_str(f"{bot_left}{top_bot*98}{bot_right}")
+        print()
+
+        print_c_str(
+            "Your pockets feel empty, and you only have the clothes on your back. What do you choose to do?")
+        print()
+
+        game_start = False
+
+    player_input = input()
+
+
+# Quit -
+    if player_input.lower() == "quit":
         quit_game()
 
 
-# While running true loop
+# Controls -
+    elif player_input.lower() == "controls":
+        ctrs_menu()
 
-# Inventory table before each action
+    elif player_input.lower() == "bag":
+        for itm, qty in plr_inv.items():
+            if qty >= 1:
+                print_c_str(f"{itm:15}: {qty}")
+
+
+# Wood Chop -
+    elif player_input.lower() == "chop":
+        if plr_inv.get("Stone Axe") >= 1:
+            player_input = input(
+                "Would you like to use your Stone Axe? (Yes/No): ")
+            print()
+
+            if player_input.lower() == "yes":
+
+                qty_added = random.randint(2, 3)
+                plr_inv["Logs"] += qty_added
+
+                print_c_str(
+                    f"You used your Stone Axe to chop down a tree got {qty_added} Logs. You now have {plr_inv.get('Logs')} Logs.")
+
+            elif player_input.lower() == "no":
+
+                qty_added = random.randint(0, 1)
+
+                print_c_str(
+                    "You chose to try to chop a tree without the Axe from your bag... ")
+                print()
+
+                if qty_added == 0:
+
+                    print_c_str(
+                        f"You punched a tree as hard as you could but nothing broke loose. You still have {plr_inv.get('Logs')} Logs.")
+                    print()
+
+                else:
+
+                    # Gain 1 Log
+                    plr_inv["Logs"] += qty_added
+
+                    print_c_str(
+                        f"You shook the tree as hard as you could and a branch fell off. You now have {plr_inv.get('Logs')} Logs.")
+        else:
+            qty_added = random.randint(0, 1)
+
+            if qty_added == 0:
+
+                print_c_str(
+                    f"You tried to karate chop the tree but, you didn't even make a dent. You still have {plr_inv.get('Logs')} Logs and a bruised hand")
+
+            else:
+
+                # Gain 1 Log
+                plr_inv["Logs"] += qty_added
+
+                print_c_str(
+                    f"You found a fallen tree and managed to break off a chunk. You now have {plr_inv.get('Logs')} Logs.")
+
+
+# Mine -
+    elif player_input.lower() == "mine":
+        pass
+
+
+# Craft -
+    elif player_input.lower() == "craft":
+        pass
+
+
+# Smelt -
+    elif player_input.lower() == "smelt":
+        pass
+
+
+# Invalid Action -
+    else:
+        print_c_str("Action entered is invalid, Try again!")
+        print_c_str(
+            "Type [controls] if you want to see actions you can perform.")
 
 
 # Actions
